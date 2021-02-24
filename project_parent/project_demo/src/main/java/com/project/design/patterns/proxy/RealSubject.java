cf0947c419626828f88a946bc41ca924c91fbe84
wjl <wujinlong@eusunpower.com>
2021-02-24 17:32:56 +0800

    .

---
+++ project_parent/project_demo/src/main/java/com/project/design/patterns/proxy/RealSubject.java
@@ -0,0 +1,8 @@
+package com.project.design.patterns.proxy;
+
+public class RealSubject implements Subject {
+    @Override
+    public void request() {
+        System.out.println("真实主题");
+    }
+}
---
+++ project_parent/project_demo/src/main/java/com/project/design/patterns/proxy/Subject.java
@@ -0,0 +1,6 @@
+package com.project.design.patterns.proxy;
+
+//抽象主题
+public interface Subject {
+    void request();
+}
