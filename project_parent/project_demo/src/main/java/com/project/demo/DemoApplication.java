1718eaaa0865bb9c184e9075a39d79499aa7f428
wjl <wujinlong@eusunpower.com>
2021-03-05 10:54:03 +0800

    .

--- project_parent/project_demo/src/main/java/com/project/demo/DemoApplication.java
+++ project_parent/project_demo/src/main/java/com/project/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.project.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
@@ -10,5 +11,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class DemoApplication {
     public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class);
     }
 }
